3017e86ef207f4a20ba63c0e9cd529797e3dd162
cosmos510 <maximemartin510@gmail.com>
2024-06-06 08:22:24 +0000

    feat: Add constructor for City model

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@ from models.base_model import BaseModel
 class City(BaseModel):
     """ City class """
     def __init__(self, name, country_id):
+        """ Constructor for City"""
         super().__init__()
         self.name = name
         self.country_id = country_id
