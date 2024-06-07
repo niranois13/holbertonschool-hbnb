f40d340110bec3b6dbf5f7f90659005311da03c2
cosmos510 <maximemartin510@gmail.com>
2024-06-07 12:37:46 +0000

    chore: Refactor add_user endpoint to handle FileNotFoundError

--- app/api/user_api.py
+++ app/api/user_api.py
@@ -1,20 +1,26 @@
 from flask import Blueprint, jsonify, request
 from models.users import User
 from persistence.datamanager import DataManager
+import json
 user_api = Blueprint("user_api", __name__)
 
-@user_api.route("/user", methods=["POST"])
+@user_api.route("/users", methods=["POST", 'GET'])
 def add_user():
-    user_data = request.get_json()
-    new_user = User(user_data["email"] , user_data["first_name"], user_data["last_name"])
-    try:
-        with open("User.json", 'r') as f:
-            if user_data["email"] in f.read():
-                return jsonify("user already exist"), 400
-    except FileNotFoundError:
-        pass
-    if not new_user:
-        return jsonify("Error setting up new user")
+    if request.method == "POST":
+        user_data = request.get_json()
+        new_user = User(user_data["email"] , user_data["first_name"], user_data["last_name"])
+        try:
+            with open("User.json", 'r') as f:
+                if user_data["email"] in f.read():
+                    return jsonify("user already exist"), 400
+        except FileNotFoundError:
+            pass
+        if not new_user:
+            return jsonify("Error setting up new user")
+        else:
+            DataManager().save(new_user.to_dict())
+            return jsonify("User added", new_user.to_dict()), 201
     else:
-        DataManager().save(new_user.to_dict())
-        return jsonify("User added", new_user.to_dict()), 201
+        with open("User.json", 'r', encoding='utf-8') as f:
+            users = json.load(f)
+        return jsonify(users), 200
--- app/persistence/datamanager.py
+++ app/persistence/datamanager.py
@@ -6,19 +6,14 @@ class DataManager():
         """
         Methdod used to save data(entity) into a JSON file
         """
-        with open("User.json", 'a', encoding='UTF-8') as f:
-            json.dump(entity,f, indent=4,)
+        file_path = "User.json"
+        data = []
+        try:
+            with open(file_path, 'r', encoding='UTF-8') as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            pass
+        data.append(entity)
 
-        
-
-    def get(self, entity_id, entity_type):
-        # Logic to retrieve an entity based on ID and type
-        pass
-
-    def update(self, entity):
-        #Logic to update an entity in storage
-        pass
-
-    def delete(self, entity_id, entity_type):
-        # Logic to delete an entity from storage
-        pass
+        with open(file_path, 'w', encoding='UTF-8') as f:
+            json.dump(data,f, indent=4)
