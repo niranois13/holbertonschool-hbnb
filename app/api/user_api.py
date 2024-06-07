e2a602e28f3bca54b7f636a183e0154ae88879cc
niranois13 <8784@holbertonstudents.com>
2024-06-07 09:58:54 +0200

    still cleaning the repo

--- app/api/user_api.py
+++ app/api/user_api.py
@@ -1,14 +1,2 @@
-from flask import jsonify, request
-from models.users import User
-from app import app
-
-@app.route("/user", methods=['POST'])
-def add_user():
-    user_data = request.get_json()
-    new_user = User(user_data)
-    if not new_user:
-        return jsonify("Error setting up new user")
-    else:
-        return jsonify("User added", new_user), 201
 
 
