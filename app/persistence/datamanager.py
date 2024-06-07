75c7eb97d520f781674899fbb4f30f4f2451744d
cosmos510 <maximemartin510@gmail.com>
2024-06-07 09:48:09 +0000

    chore: Add endpoint to add new user

--- app/persistence/datamanager.py
+++ app/persistence/datamanager.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+import json
 
 class DataManager():
     """Defines the subclass DataManager that inherits from IPersistenceManager"""
@@ -6,7 +6,10 @@ class DataManager():
         """
         Methdod used to save data(entity) into a JSON file
         """
-        return jsonify(entity)
+        with open("User.json", 'a', encoding='UTF-8') as f:
+            json.dump(entity,f, indent=4,)
+
+        
 
     def get(self, entity_id, entity_type):
         # Logic to retrieve an entity based on ID and type
