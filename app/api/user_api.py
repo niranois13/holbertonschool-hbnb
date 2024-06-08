fc13b57448802fc81241353762b44d270b161710
niranois13 <8784@holbertonstudents.com>
2024-06-08 09:35:07 +0200

    Handle flags for json files

--- app/api/user_api.py
+++ app/api/user_api.py
@@ -4,11 +4,12 @@ from persistence.datamanager import DataManager
 from validate_email_address import validate_email
 import json
 user_api = Blueprint("user_api", __name__)
+datamanager = DataManager(flag=1)
 
 @user_api.route("/users", methods=["POST", 'GET'])
 def add_user():
     """
-    Function used to create a new user, send it to the database manager
+    Function used to create a new user, send it to the database datamanager
     and read a list of existing users.
     """
     if request.method == "POST":
@@ -41,15 +42,18 @@ def add_user():
         if not new_user:
             return jsonify({"Error": "setting up new user"}), 500
         else:
-            DataManager().save(new_user.to_dict(),1)
+            datamanager.save(new_user.to_dict())
             return jsonify({"Success": "User added"}, new_user.to_dict()), 201
 
     else:
         #Bloc that handles the GET method by reading and returning the data
         #stored in the database
-        with open("User.json", 'r', encoding='utf-8') as f:
-            users = json.load(f)
-        return jsonify(users), 200
+        try:
+            with open("User.json", 'r', encoding='utf-8') as f:
+                users = json.load(f)
+                return jsonify(users), 200
+        except FileNotFoundError:
+            return jsonify({"Error": "No user found"}), 404
 
 @user_api.route("/users/<string:id>", methods=['GET', 'DELETE','PUT'])
 def get_user(id):
@@ -60,7 +64,7 @@ def get_user(id):
     if request.method == 'GET':
         #Bloc that handles the GET mehod of the data of a specifi user
         #by returning this data, pulles from the database.
-        users = DataManager().get("User",id,1)
+        users = datamanager.get("User",id,1)
         if not users:
             return jsonify({"Error": "User not found"}), 404
         return jsonify(users), 200
@@ -68,24 +72,24 @@ def get_user(id):
     if request.method == 'DELETE':
         #Bloc that handles the DELETE method by pulling data specific to a user
         #and calls the DataManager function to do the removal
-        users = DataManager().get("User",id,1)
+        users = datamanager.get("User",id)
         if not users:
             return jsonify({"Error": "User not found"}), 404
-        users = DataManager().delete("User",id,1)
+        users = datamanager.delete("User",id)
         if not users:
             return jsonify({"Success": "User deleted"}), 200
-        
+
 
 
     if request.method == 'PUT':
         #Bloc that handles the PUT method by pulling data of a specific user
         #and calling th DataManager to update the database
         user_data = request.get_json()
-        user = DataManager().get("User",id,1)
+        user = datamanager.get("User",id)
         if not user:
             return jsonify({"Error": "User not found"}), 404
         user["email"] = user_data["email"]
         user["first_name"] = user_data["first_name"]
         user["last_name"] = user_data["last_name"]
-        DataManager().update(user, id, 1)
+        datamanager.update(user, id)
         return jsonify({"Success": "User updated"}, user), 200
