650c0d03b1b535c1ba2c5f0bbdfd4ddb7b33719b
niranois13 <8784@holbertonstudents.com>
2024-06-10 16:03:06 +0200

    adding review

---
+++ app/api/review_api.py
@@ -0,0 +1,55 @@
+from flask import Blueprint, jsonify, request
+from models.review import Review
+from persistence.datamanager import DataManager
+import json
+review_api = Blueprint("review_api", __name__)
+datamanager = DataManager(flag=5)
+
+
+@review_api.route("/places/<string:id>/reviews", methods=["POST", "GET"])
+def handle_place_review(id):
+    """
+    Function used to create and retriew reviews of a place
+    """
+    if request.method == "POST":
+        review_data = request.get_json()
+        if not review_data:
+            return jsonify({"Error": "Problem during review creation"}), 400
+
+        rating = review_data.get("rating")
+        if not isinstance(rating, int):
+            return jsonify({"Error": "rating must be an integer."}), 400
+        if not 1 <= rating <= 5:
+            return jsonify({"Error":
+                            "rating must be included between 1 and 5."}), 400
+
+        comment = review_data.get("comment")
+        if not isinstance(comment, str):
+            return jsonify({"Error": "comment must be a string."}), 400
+
+        try:
+            with open("User.json", 'r', encoding='UTF-8') as f:
+                users = json.load(f)
+                user_id = review_data.get("user_id")
+                if user_id in users:
+                    pass
+                else:
+                    return jsonify({"Error": "User not found"}), 404
+        except Exception as e:
+            return jsonify({"Error": str(e)}), 404
+
+        place_id = id
+        new_review = Review(user_id, place_id, rating, comment)
+        if not new_review:
+            return jsonify({"Error": "adding new review"}), 500
+        else:
+            datamanager.save(new_review.to_dict())
+            return jsonify({"Success": "Review added"}), 201
+
+    else:
+        try:
+            with open("Review.json", 'r', encoding='UTF-8') as f:
+                reviews = json.load(f)
+                return jsonify(reviews), 200
+        except FileNotFoundError:
+            return jsonify({"Error": "Review not found"}), 404
