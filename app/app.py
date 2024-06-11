9a4eb91d3c84eaa0272437022869e67fbae30ceb
cosmos510 <maximemartin510@gmail.com>
2024-06-11 07:17:27 +0000

    chore: Update Swagger UI configuration and registration

--- app/app.py
+++ app/app.py
@@ -4,7 +4,21 @@ from api.country_api import country_api
 from api.place_api import place_api
 from api.amenities_api import amenities_api
 from api.review_api import review_api
+from flask_swagger_ui import get_swaggerui_blueprint
 app = Flask(__name__)
+
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.json'
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+)
+
+app.register_blueprint(swaggerui_blueprint)
 app.register_blueprint(user_api)
 app.register_blueprint(country_api)
 app.register_blueprint(place_api)
--- app/models/place.py
+++ app/models/place.py
@@ -3,7 +3,7 @@ from models.base_model import BaseModel
 
 class Place(BaseModel):
     """Defines class Place that inherits from BaseModel"""
-    def __init__(self, name, description, address, city_id, lattitude,
+    def __init__(self, name, description, address, city_id, latitude,
                 longitude, host_id, num_rooms, num_bathrooms,
                 price_per_night, max_guests, amenity_ids):
         """Initializes the class Place with the following parameters:
@@ -24,11 +24,11 @@ class Place(BaseModel):
         self.description = description
         self.address = address
         self.city_id = city_id
-        self.lattitude = lattitude
+        self.latitude = latitude
         self.longitude = longitude
         self.host_id = host_id
         self.num_rooms = num_rooms
         self.num_bathrooms = num_bathrooms
-        self.price_per_nght = price_per_night
+        self.price_per_night = price_per_night
         self.max_guests = max_guests
         self.amenity_ids = amenity_ids
